37bbfa88c6976d917852a1f034c00292fb91c26d
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-31 04:34:16 -0700

    pictures

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -24,7 +24,7 @@ function ShowCase() {
     <img src="/public/img-1.webp" alt="" className="w-full hover:blur-md bg-cover" />
   </div>
   <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <video src="/public/vid-3.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
+  <img src="/public/img-8.webp" alt="" className="w-full h-full hover:blur-md bg-cover" />
   </div>
 </div>
 <div className='flex justify-center flex-wrap mb-9'>
@@ -48,7 +48,7 @@ function ShowCase() {
     <img src="/public/img-11.webp" alt="" className="w-full hover:blur-md bg-cover" />
   </div>
   <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover" autoPlay loop playsInline />
+    <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
   </div>
 </div>
 
