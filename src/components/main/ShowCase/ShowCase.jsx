a875e698311ccda6981de9920ef198b7d0f433fb
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-31 04:35:08 -0700

    clean

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -12,46 +12,45 @@ function ShowCase() {
           </div>
         </div>
         <div className='flex justify-center flex-wrap mb-9'>
-  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-    <img src="/public/img-3.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <img src="/public/img-2.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-</div>
-<div className='flex justify-center flex-wrap mb-9'>
-  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-    <img src="/public/img-1.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-  <img src="/public/img-8.webp" alt="" className="w-full h-full hover:blur-md bg-cover" />
-  </div>
-</div>
-<div className='flex justify-center flex-wrap mb-9'>
-  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-    <img src="/public/img-4.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <video src="/public/vid-2.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
-  </div>
-</div>
-<div className='flex justify-center flex-wrap mb-9'>
-  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-    <img src="/public/img-10.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <img src="/public/img-9.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-</div>
-<div className='flex justify-center flex-wrap mb-9'>
-  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-    <img src="/public/img-11.webp" alt="" className="w-full hover:blur-md bg-cover" />
-  </div>
-  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-    <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
-  </div>
-</div>
-
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-3.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+            <img src="/public/img-2.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-1.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+          <img src="/public/img-8.webp" alt="" className="w-full h-full hover:blur-md bg-cover" />
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-4.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+            <video src="/public/vid-2.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-10.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+            <img src="/public/img-9.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-11.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+            <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
+          </div>
+        </div>
       </div>
     </div>
   )
