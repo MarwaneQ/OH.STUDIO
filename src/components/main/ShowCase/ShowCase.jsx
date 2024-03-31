fb26ad556891281086598b2518f7a16fd12e11ee
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-31 04:24:14 -0700

    hover effect

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -4,29 +4,54 @@ function ShowCase() {
     <div>
       <div className="flex flex-wrap">
         <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-5.webp" alt="" className="w-1/3 rounded-2xl mr-3 hover:blur-md" />
-          <img src="/public/img-6.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
+          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+          <img src="/public/img-5.webp" alt="" className="w-full   hover:blur-md bg-cover " />
+          </div>
+          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+          <img src="/public/img-6.webp" alt="" className="w-full   hover:blur-md bg-cover " />
+          </div>
         </div>
         <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-3.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
-          <img src="/public/img-2.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-1.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
-          <video src="/public/vid-3.mp4" alt="" className="w-1/3 rounded-2xl ml-2" ></video>
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-4.webp" alt="" className="w-1/3 rounded-2xl mr-3"/>
-          <video src="/public/vid-2.mp4" alt="" className="w-1/3 rounded-2xl ml-2"></video>
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-10.webp" alt="" className="w-1/3 rounded-2xl mr-3"/>
-          <img src="/public/img-9.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <img src="/public/img-11.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
-          <video src="/public/vid-1.mp4" className="w-1/3 rounded-2xl ml-2"></video>
-        </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+    <img src="/public/img-3.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+    <img src="/public/img-2.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+</div>
+<div className='flex justify-center flex-wrap mb-9'>
+  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+    <img src="/public/img-1.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+    <video src="/public/vid-3.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
+  </div>
+</div>
+<div className='flex justify-center flex-wrap mb-9'>
+  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+    <img src="/public/img-4.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+    <video src="/public/vid-2.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
+  </div>
+</div>
+<div className='flex justify-center flex-wrap mb-9'>
+  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+    <img src="/public/img-10.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+    <img src="/public/img-9.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+</div>
+<div className='flex justify-center flex-wrap mb-9'>
+  <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
+    <img src="/public/img-11.webp" alt="" className="w-full hover:blur-md bg-cover" />
+  </div>
+  <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
+    <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover" autoPlay loop playsInline />
+  </div>
+</div>
+
       </div>
     </div>
   )
