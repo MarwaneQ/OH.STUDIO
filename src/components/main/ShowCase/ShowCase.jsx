fba67f8f5ca7a4ec0cdea5ebb990254d29ea7a3f
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-31 03:37:00 -0700

    pictures

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -2,19 +2,31 @@
 function ShowCase() {
   return (
     <div>
-      <div className="grid-cols-2 grid-rows-10	">
-         <img src="/public/img-1.webp" alt="" className="w-1/3" />
-         <img src="/public/img-2.webp" alt="" className="w-1/3" />
-         <img src="/public/img-3.webp" alt="" className="w-1/3" />
-         <img src="/public/img-4.webp" alt="" className="w-1/3" />
-         <img src="/public/img-5.webp" alt="" className="w-1/3" />
-         <img src="/public/img-6.webp" alt="" className="w-1/3" />
-         <img src="/public/img-9.webp" alt="" className="w-1/3" />
-         <img src="/public/img-10.webp" alt="" className="w-1/3" />
-         <img src="/public/img-11.webp" alt="" className="w-1/3" />
-         <video src="/public/vid-1.mp4" className="w-1/3"></video>
-         <video src="/public/vid-2.mp4" className="w-1/3"></video>
-         <video src="/public/vid-3.mp4" className="w-1/3"></video>
+      <div className="flex flex-wrap">
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-5.webp" alt="" className="w-1/3 rounded-2xl mr-3 hover:blur-md" />
+          <img src="/public/img-6.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-3.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
+          <img src="/public/img-2.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-1.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
+          <video src="/public/vid-3.mp4" alt="" className="w-1/3 rounded-2xl ml-2" ></video>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-4.webp" alt="" className="w-1/3 rounded-2xl mr-3"/>
+          <video src="/public/vid-2.mp4" alt="" className="w-1/3 rounded-2xl ml-2"></video>
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-10.webp" alt="" className="w-1/3 rounded-2xl mr-3"/>
+          <img src="/public/img-9.webp" alt="" className="w-1/3 rounded-2xl ml-2" />
+        </div>
+        <div className='flex justify-center flex-wrap mb-9'>
+          <img src="/public/img-11.webp" alt="" className="w-1/3 rounded-2xl mr-3" />
+          <video src="/public/vid-1.mp4" className="w-1/3 rounded-2xl ml-2"></video>
+        </div>
       </div>
     </div>
   )
