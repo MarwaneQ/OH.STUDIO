baa156d7ec6835ed252476a0190ef2f50bcc1b2f
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-31 06:08:39 -0700

    responseve

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -3,52 +3,52 @@ function ShowCase() {
   return (
     <div>
       <div className="flex flex-wrap md:mt-44">
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-          <img src="/public/img-5.webp" alt="" className="w-full   hover:blur-md bg-cover " />
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0 '>
+          <div className="w-full  md:w-1/3 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+          <img src="/public/img-5.webp" alt="" className="w-full  transition duration-400	 hover:blur-md bg-cover " />
           </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-          <img src="/public/img-6.webp" alt="" className="w-full   hover:blur-md bg-cover " />
-          </div>
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-            <img src="/public/img-3.webp" alt="" className="w-full hover:blur-md bg-cover" />
-          </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-            <img src="/public/img-2.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+          <img src="/public/img-6.webp" alt="" className="w-full transition duration-400 hover:blur-md bg-cover " />
           </div>
         </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-            <img src="/public/img-1.webp" alt="" className="w-full hover:blur-md bg-cover" />
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-3.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-          <img src="/public/img-8.webp" alt="" className="w-full h-full hover:blur-md bg-cover" />
-          </div>
-        </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-            <img src="/public/img-4.webp" alt="" className="w-full hover:blur-md bg-cover" />
-          </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-            <video src="/public/vid-2.mp4" alt="" className="w-full hover:blur-md bg-cover" autoPlay loop playsInline/>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+            <img src="/public/img-2.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
         </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-            <img src="/public/img-10.webp" alt="" className="w-full hover:blur-md bg-cover" />
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-1.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-            <img src="/public/img-9.webp" alt="" className="w-full hover:blur-md bg-cover" />
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+          <img src="/public/img-8.webp" alt="" className="w-full h-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
         </div>
-        <div className='flex justify-center flex-wrap mb-9'>
-          <div className="w-1/3 overflow-hidden rounded-2xl mr-3">
-            <img src="/public/img-11.webp" alt="" className="w-full hover:blur-md bg-cover" />
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-4.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
-          <div className="w-1/3 overflow-hidden rounded-2xl ml-2">
-            <video src="/public/vid-1.mp4" alt="" className="w-full hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+            <video src="/public/vid-2.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" autoPlay loop playsInline/>
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-10.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+            <img src="/public/img-9.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+          </div>
+        </div>
+        <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl mr-3">
+            <img src="/public/img-11.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl ml-2">
+            <video src="/public/vid-1.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
           </div>
         </div>
       </div>
