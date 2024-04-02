e38c0768ba316cad196f9f2c0b465e24e76171d6
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-04-02 11:14:33 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,8 @@ import './Header.css'
 function Header() {
   return (
     <>
-    <header className="flex  bg-transparent  justify-center w-full">
+    <header className=" flex  bg-transparent  justify-center w-full">
+      <div className="fixed  z-50">
       <nav className="p-6 md:w-[900px]  flex justify-center">
         <ul className="flex lg:justify-around backdrop-blur-md bg-[#e8e5e480] p-2 w-[270px] md:w-1/3 justify-around lg:w-2/6 lg:h-12 items-center   rounded-full">
           <div className="">
@@ -23,6 +24,7 @@ function Header() {
           </div>
         </ul>
       </nav>
+      </div>
     </header>
     <Outlet />
     </>
