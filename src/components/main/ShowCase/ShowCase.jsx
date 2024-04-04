c7db044844b96024ad79bd37b87245a06886c2e6
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-04-04 01:29:51 -0700

    image error fixed

--- src/components/main/ShowCase/ShowCase.jsx
+++ src/components/main/ShowCase/ShowCase.jsx
@@ -12,50 +12,50 @@ function ShowCase() {
       <div className={`flex flex-wrap md:mt-44 justify-center`}>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0 '>
           <div className={`w-full  md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-          <img src="public/img-5.webp" alt="" className="w-full  transition duration-400	 hover:blur-md bg-cover " />
+          <img src="p" alt="" className="w-full  transition duration-400	 hover:blur-md bg-cover " />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-          <img src="public/img-6.webp" alt="" className="w-full transition duration-400 hover:blur-md bg-cover " />
+          <img src="/img-6.webp" alt="" className="w-full transition duration-400 hover:blur-md bg-cover " />
           </div>
         </div>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-3.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-3.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-2.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-2.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
         </div>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-1.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-1.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-          <img src="public/img-8.webp" alt="" className="w-full h-full transition duration-400	 hover:blur-md bg-cover" />
+          <img src="/img-8.webp" alt="" className="w-full h-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
         </div>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
           <div className={`w-full  md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-4.webp" alt="" className="w-full transition duration-400 h-full	 hover:blur-md bg-cover" />
+            <img src="/img-4.webp" alt="" className="w-full transition duration-400 h-full	 hover:blur-md bg-cover" />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-            <video src="public/vid-2.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" autoPlay loop playsInline/>
+            <video src="/vid-2.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" autoPlay loop playsInline/>
           </div>
         </div>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
           <div className={`w-full  md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-10.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-10.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-9.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-9.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
         </div>
         <div className='flex justify-center flex-wrap md:mb-9 p-5 md:p-0'>
           <div className={`w-full  md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:mr-3 hero-text ${loaded ? 'animate' : ''}`}>
-            <img src="public/img-11.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
+            <img src="/img-11.webp" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover" />
           </div>
           <div className={`w-full md:w-1/2 lg:w-1/3 overflow-hidden rounded-2xl md:ml-2 mt-8 md:mt-0 hero-text ${loaded ? 'animate' : ''}`}>
-            <video src="public/vid-1.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
+            <video src="/vid-1.mp4" alt="" className="w-full transition duration-400	 hover:blur-md bg-cover object-cover h-full" autoPlay loop playsInline />
           </div>
         </div>
         <div className=" flex justify-center items-center">
